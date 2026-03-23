907e2952a7cb8a5b05887ae0b681e6e276c6b9d3
Catalin <catalinflorin.barbu@ulbsibiu.ro>
2026-03-23 17:19:12 +0200

    problema 3 corectata si functionala

--- problema3.py
+++ problema3.py
@@ -1,14 +1,24 @@
 import random
 
 
-numar_secret=int(random.randint(1,50))
-incercari=0
-print("Am ales un numar intre 1 si 50")
-int(input("am incercat numarul: "))
+numar_secret = random.randint(1, 50)
+incercari = 0
+
+print("Am ales un număr între 1 și 50. Încearcă să-l ghicești!")
 
 while True:
 
-    if numar_secret>input:
-        print("numarul este mai mic")
+    try:
+        ghicit = int(input("Introdu un număr: "))
+        incercari += 1
 
+        if ghicit < numar_secret:
+            print("Numărul este mai mare!")
+        elif ghicit > numar_secret:
+            print("Numărul este mai mic!")
+        else:
+            print("Felicitări! Ai ghicit numărul {numar_secret} din {incercari} încercări!")
+            break
 
+    except ValueError:
+        print("Te rog să introduci un număr valid (întreg).")
